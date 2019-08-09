1a36dbbf9a05bf6c8c31788ad4a8ac17fc076f83
Rajesh Seenivasan <rajesh.seenivasan@microchip.com>
2019-08-09 12:52:17 +0530

    first commit

---
+++ serverevent/src/browser/serverevent-frontend-module.ts
@@ -0,0 +1,31 @@
+import { ContainerModule } from 'inversify';
+import { ServereventWidget } from './serverevent-widget';
+import { ServereventContribution } from './serverevent-contribution';
+import { bindViewContribution, FrontendApplicationContribution, WidgetFactory, WebSocketConnectionProvider } from '@theia/core/lib/browser';
+
+import '../../src/browser/style/index.css';
+import { TestServer, testPath, ReconnectingFileSystemWatcherServer, TestServerProxy } from '../common/test-protocol';
+
+
+export default new ContainerModule(bind => {
+
+  
+
+
+    bind(TestServerProxy).toDynamicValue(ctx =>
+        WebSocketConnectionProvider.createProxy(ctx.container, testPath)
+    );
+    bind(TestServer).to(ReconnectingFileSystemWatcherServer);
+        bindViewContribution(bind, ServereventContribution);
+
+        
+    bind(FrontendApplicationContribution).toService(ServereventContribution);
+    bind(ServereventWidget).toSelf().inSingletonScope();
+    bind(WidgetFactory).toDynamicValue(ctx => ({
+        id: ServereventWidget.ID,
+        createWidget: () => ctx.container.get<ServereventWidget>(ServereventWidget)
+    })).inSingletonScope();
+
+
+
+});
